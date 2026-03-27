f4e6abba74b7011a6457838955c309290c3538e2
Mounika Padigala <mounika.padigala@veniso.com>
2026-03-27 13:41:49 +0530

    test

--- test.php
+++ test.php
@@ -1,4 +1,3 @@
 sakc nfskcn d
 curl -i -u admin:admin1233 http://localhost:15672/api/views
-curl -i -u admin:admin1233 http://localhost:15672/api/views
-curl -i -u admin:admin1233 http://localhost:15672/api/views
+curl -i -u admin:admin12334444 http://localhost:15672/api/views

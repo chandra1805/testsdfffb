dd63e58fdef7a820e7dfaa135176a3577155212f
Mounika Padigala <mounika.padigala@veniso.com>
2026-04-01 11:29:53 +0530

    this is for testing

--- test.php
+++ test.php
@@ -2,3 +2,4 @@ sakc nfskcn d
 curl -i -u admin:admin1233 http://localhost:15672/api/views
 curl -i -u admin:admin12334444 http://localhost:15672/api/views
 curl -i -u admin:admin124444 http://localhost:15672/api/views
+curl -i -u admin:admin124444 http://localhost:15672/api/views
